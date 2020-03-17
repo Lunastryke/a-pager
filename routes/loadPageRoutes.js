9c2faffe5ed8c55c4cf079311681f590c70036d1
Francis <francislow_6@hotmail.com>
2020-03-17 20:10:13 +0800

    Quick fix for initial redirect

--- routes/loadPageRoutes.js
+++ routes/loadPageRoutes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 // All routes other than '/' route will be directed to amazon
-router.route('/*').get((req, res) => {
-  res.redirect('http://www.amazon.com' + req.url);
+router.route('/gp/*').get((req, res) => {
+  res.redirect('http://www.amazon.sg' + req.url);
+});
+
+router.route('/ap/*').get((req, res) => {
+  res.redirect('http://www.amazon.sg' + req.url);
 });
 
 module.exports = router;
