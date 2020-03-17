2c12376a3bc93976702d69ac6723346501ba236e
Francis <francislow_6@hotmail.com>
2020-03-17 13:49:28 +0800

    Finalise fixed for routing bug

--- routes/loadPageRoutes.js
+++ routes/loadPageRoutes.js
@@ -8,12 +8,7 @@ router.route('/gp/*').get((req, res) => {
 // Testing
 // Test site: https://github.com/Lunastryke/a-pager
 router.route('/Lunastryke/*').get((req, res) => {
-  res.send('response ok');
   res.redirect('http://www.github.com' + req.url);
-});
-
-router.route('/api/get_reponse').get((req, res) => {
-  res.send('response sent');
 });
 
 module.exports = router;
